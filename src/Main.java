5e7bb4108f338fa972b801db5a9e56e7bd7d25b0
Daryn Mulholland <darynmulholland@Daryns-MBP.hitronhub.home>
2018-09-30 19:41:15 -0700

    added number of walls

--- src/Main.java
+++ src/Main.java
@@ -13,22 +13,29 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<action> actions = new ArrayList<action>();
         ArrayList<observation> observations = new ArrayList<observation>();
+        // TODO Starting state initializer flag
 
         // Set Up Board (default settings of 1/9 for each belief state, except terminal states):
-        State AA = new State(1,1,(float) 1/9);
-        State BA = new State(2,1,(float) 1/9);
-        State CA = new State(3,1,(float) 1/9);
-        State DA = new State(4,1,(float) 1/9);
+        State AA = new State(1,1,(float) 1/9, 2);
+        State BA = new State(2,1,(float) 1/9, 2);
+        State CA = new State(3,1,(float) 1/9, 1);
+        State DA = new State(4,1,(float) 1/9, 2);
 
-        State AB = new State(1,2,(float) 1/9);
-        // State BB = new State(2,2,1/9);  // Not a state
-        State CB = new State(3,2,(float) 1/9);
-        State DB = new State(4,2,0); // Terminal State
+        State AB = new State(1,2,(float) 1/9, 2);
+        // State BB // Not a state
+        State CB = new State(3,2,(float) 1/9, 1);
+        State DB = new State(4,2,0, 0); // Terminal State
 
-        State AC = new State(1,3,(float) 1/9);
-        State BC = new State(2,3,(float) 1/9);
-        State CC = new State(3,3,(float) 1/9);
-        State DC = new State(4,3,0); // Terminal State
+        State AC = new State(1,3,(float) 1/9, 2);
+        State BC = new State(2,3,(float) 1/9, 2);
+        State CC = new State(3,3,(float) 1/9, 1);
+        State DC = new State(4,3,0, 0 ); // Terminal State
+
+        // Readjust belief state when starting state is specified
+        // TODO
+
+        // Set Probability Distribution of Starting State:
+        // TODO: AA.setBelief(1);
 
         // Actions to input
         actions.add(action.UP);
--- src/State.java
+++ src/State.java
@@ -2,19 +2,24 @@ public class State {
     private int x;
     private int y;
     private double belief;
+    private int numberOfWalls;
 
-    public State(int x, int y, double belief) {
+    public State(int x, int y, double belief, int numberOfWalls) {
         this.x = x;
         this.y = y;
         this.belief = belief;
+        this.numberOfWalls = numberOfWalls;
+
     }
+
+    // TODO something with the number of walls
 
     public void setBelief(double newBelief) {
         this.belief = newBelief;
     }
 
     public void printState() {
-        System.out.println("State (" + x + "," + y + "); belief = " + belief);
+        System.out.println("State (" + x + "," + y + "); belief = " + belief + "; #walls = " + numberOfWalls);
 
     }
 }
